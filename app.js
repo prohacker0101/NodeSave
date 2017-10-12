348f694fa54aedea0220c058f3f46bf273a61f47
prohacker0101 <32649697+prohacker0101@users.noreply.github.com>
2017-10-12 07:45:16 -0500

    start

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express'),
+    exphbs  = require('express3-handlebars'),
+
+    app = express();
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/about', function (req, res) {
+    res.render('about');
+});
+
+app.listen(3000);
